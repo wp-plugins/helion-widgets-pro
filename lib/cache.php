c7dc7c1d2cf2c1de2118c77c3fe0a6d22d0651a0
paulpela <paulpela@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-30 10:24:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/helion-widgets-pro/trunk@417135 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/cache.php
+++ lib/cache.php
@@ -215,15 +215,15 @@ function helion_setup_cache() {
 	$bookstores = array("helion", "sensus", "onepress", "septem");
 	$covers = array("326x466", "181x236", "125x163", "120x156", "90x119", "88x115", "72x95", "65x85");
 	
-	mkdir(ABSPATH . "wp-content/helion-cache", 0775);
+	@mkdir(ABSPATH . "wp-content/helion-cache", 0775);
 	foreach($bookstores as $bookstore) {
-		mkdir(ABSPATH . "wp-content/helion-cache/" . $bookstore, 0775);
+		@mkdir(ABSPATH . "wp-content/helion-cache/" . $bookstore, 0775);
 		foreach($covers as $cover) {
-			mkdir(ABSPATH . "wp-content/helion-cache/" . $bookstore . "/" . $cover, 0775);
+			@mkdir(ABSPATH . "wp-content/helion-cache/" . $bookstore . "/" . $cover, 0775);
 		}
 	}
 	
-	mkdir(ABSPATH . "wp-content/helion-cache/xml");
+	@mkdir(ABSPATH . "wp-content/helion-cache/xml");
 }
 
 /**
