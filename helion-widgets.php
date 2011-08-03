f345bbb2ea36afeeccf567df9c6f445b08d9a23a
paulpela <paulpela@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-03 12:33:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/helion-widgets-pro/tags/0.96@418717 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ helion-widgets.php
@@ -0,0 +1,216 @@
+<?php
+/*
+	Plugin Name: Helion Widgets Pro
+	Plugin URI: http://wordpress.org/extend/plugins/helion-widgets-pro/
+	Description: Widgety i Księgarnia dla uczestników Programu Partnerskiego GW Helion.
+	Version: 0.96
+	Author: Paweł Pela
+	Author URI: http://paulpela.com/
+	License: GPL2
+*/
+
+include_once("lib/lib.php");
+include_once("widgets/widgets.php");
+
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+register_activation_hook(__FILE__, "helion_install");
+
+function helion_install() {
+	global $wpdb;
+	
+	$bookstores = array("helion", "sensus", "onepress", "septem");
+	
+	foreach($bookstores as $bookstore) {
+		$table_name = $wpdb->prefix . "helion_books_" . $bookstore;
+		
+		if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+		  
+			$sql = "CREATE TABLE IF NOT EXISTS " . $table_name . " (
+				id mediumint(9) NOT NULL AUTO_INCREMENT,
+				ident text NULL,
+				isbn text NULL,
+				tytul text NULL,
+				tytul_orig text NULL,
+				link text NULL,
+				autor text NULL,
+				tlumacz text NULL,
+				cena text NULL,
+				cenadetaliczna text NULL,
+				znizka text NULL,
+				marka text NULL,
+				nazadanie bool NULL,
+				format text NULL,
+				liczbastron text NULL,
+				oprawa text NULL,
+				nosnik text NULL,
+				datawydania text NULL,
+				issueurl text NULL,
+				online text NULL,
+				bestseller bool NULL,
+				nowosc bool NULL,
+				videos text NULL,
+				powiazane text NULL,
+				opis text NULL,
+				kategorie text NULL,
+				seriewydawnicze text NULL,
+				serietematyczne text NULL,
+				UNIQUE KEY id (id)
+			) CHARACTER SET latin2;";
+
+			dbDelta($sql);
+		}
+		
+	}
+	
+	$widgets = array("random", "bookstore");
+	
+	foreach($widgets as $widget) {
+		$table_name = $wpdb->prefix . "helion_widget_" . $widget;
+		
+		if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+		  
+			$sql = "CREATE TABLE " . $table_name . " (
+				id mediumint(9) NOT NULL AUTO_INCREMENT,
+				typ text NOT NULL,
+				obiekt text NOT NULL,
+				UNIQUE KEY id (id)
+			) CHARACTER SET latin2;";
+
+			dbDelta($sql);
+		}
+		
+	}
+	
+	$table_name = $wpdb->prefix . "helion_bestsellers";
+	if($wpdb->get_var("show tables like '$table_name'") != $table_name) {
+	  
+		$sql = "CREATE TABLE " . $table_name . " (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			bookstore text NOT NULL,
+			ident text NOT NULL,
+			UNIQUE KEY id (id)
+		) CHARACTER SET latin2;";
+
+		dbDelta($sql);
+	}
+	
+	@helion_setup_cache();
+
+}
+
+function helion_activation() {
+	wp_schedule_event(time() + 12 * 60 * 60, 'daily', 'helion_download_xmls');
+	wp_schedule_event(time() + 12 * 60 * 60 + 600, 'daily', 'helion_import_xmls');
+	
+	wp_schedule_event(time() + 12 * 60 * 60 + 3600, 'daily', 'helion_download_bestsellers');
+	wp_schedule_event(time() + 12 * 60 * 60 + 600 + 3600, 'daily', 'helion_import_bestsellers');
+	
+	wp_schedule_event(time() + 61800, 'daily', 'helion_cron_cache_size');
+	
+	wp_schedule_single_event(time() + 30 * 24 * 60 * 60, 'helion_reset_cache');
+	
+	@helion_clear_bestsellers();
+	
+	$bs = array("helion","onepress", "sensus", "septem");
+	foreach($bs as $b) {
+		@helion_xml_download($b, true);
+		@helion_xml_import($b, true);
+	}
+	
+	if(!get_option("helion_wyszukiwarka_template")) {
+	
+		$template = '<div class="helion_ksiazka"><div style="float: left; width: 181px; margin-right: 20px;"><a href="%dokoszyka%">%okladka181x236%</a></div><div style="float: left; width: 350px;"><h2><a href="%dokoszyka%">%tytul%</a></h2><p>autor: %autor%</p><p>format: %format%</p><p>data wydania: %datawydania%</p><div class="helion-box"><div class="helion-cena">%cena% zł</div><a href="%dokoszyka%">kup teraz</a></div></div><div style="clear: both;"></div><hr/><div>%opis%</div><div class="helion-box"><div class="helion-cena">%cena% zł</div><a href="%dokoszyka%">kup teraz</a></div></div>';
+		
+		update_option("helion_wyszukiwarka_template", $template);
+	}
+	
+	if(!get_option("helion_bookstore_template_main")) {
+	
+		$template = '<h2>Nowości</h2>\n%nowosci%\n<hr/>\n<h2>Bestsellery</h2>\n%bestsellery%';
+		
+		update_option("helion_bookstore_template_main", $template);
+	}
+		
+	update_option("helion_bookstore_template_category", '%kategoria% %paginacja%');
+	
+	if(!get_option("helion_bookstore_template_book")) {
+	
+		$template = '<div class="helion_ksiazka"><div style="float: left; width: 181px; margin-right: 20px;"><a href="%dokoszyka%">%okladka181x236%</a></div><div style="float: left; width: 350px;"><h2><a href="%dokoszyka%">%tytul%</a></h2><p>autor: %autor%</p><p>format: %format%</p><p>data wydania: %datawydania%</p><div class="helion-box"><div class="helion-cena">%cena% zł</div><a href="%dokoszyka%">kup teraz</a></div></div><div style="clear: both;"></div><hr/><div>%opis%</div><div class="helion-box"><div class="helion-cena">%cena% zł</div><a href="%dokoszyka%">kup teraz</a></div></div>';
+		
+		update_option("helion_bookstore_template_book", $template);
+	}
+}
+
+function helion_deactivation() {
+	$timestamp = wp_next_scheduled('helion_download_xmls');
+    wp_unschedule_event($timestamp, 'helion_download_xmls');
+	$timestamp = wp_next_scheduled('helion_import_xmls');
+    wp_unschedule_event($timestamp, 'helion_import_xmls');
+	
+	$timestamp = wp_next_scheduled('helion_download_bestsellers');
+    wp_unschedule_event($timestamp, 'helion_download_bestsellers');
+	$timestamp = wp_next_scheduled('helion_import_bestsellers');
+    wp_unschedule_event($timestamp, 'helion_import_bestsellers');
+	
+	$timestamp = wp_next_scheduled('helion_cron_cache_size');
+    wp_unschedule_event($timestamp, 'helion_cron_cache_size');
+	
+	$timestamp = wp_next_scheduled('helion_reset_cache');
+    wp_unschedule_event($timestamp, 'helion_reset_cache');
+	
+}
+
+register_activation_hook(__FILE__, 'helion_activation');
+register_deactivation_hook(__FILE__, 'helion_deactivation');
+add_action('helion_download_xmls', 'helion_download_xmls');
+add_action('helion_download_bestsellers', 'helion_download_bestsellers');
+add_action('helion_import_xmls', 'helion_import_xmls');
+add_action('helion_import_bestsellers', 'helion_import_bestsellers');
+add_action('helion_cron_cache_size', 'helion_cron_cache_size');
+add_action('helion_reset_cache', 'helion_reset_cache');
+
+register_uninstall_hook(__FILE__, "helion_uninstall");
+
+function helion_uninstall() {
+	global $wpdb;
+	
+	$bookstores = array("helion", "sensus", "onepress", "septem");
+	
+	foreach($bookstores as $bookstore) {
+		$xml_table = $wpdb->prefix . "helion_books_" . $bookstore;
+		$wpdb->query("DROP TABLE " . $xml_table);
+	}
+	
+	$wpdb->query("DROP TABLE " . $wpdb->prefix . "helion_widget_bookstore");
+	$wpdb->query("DROP TABLE " . $wpdb->prefix . "helion_widget_random");
+	$wpdb->query("DROP TABLE " . $wpdb->prefix . "helion_bestsellers");
+	
+	// TODO: usunąć wszystkie opcje
+	$helion_options = array(
+		"helion_bookstores", "helion_partner_id", "helion_bookstore_template_main",
+		"helion_cache_max",
+		);
+	
+	foreach($helion_options as $o) {
+		delete_option($o);
+	}
+	
+	helion_clear_cache();
+	
+	helion_clear_bestsellers();
+}
+
+function helion_admin_scripts() {
+	wp_enqueue_script('book_selector', plugins_url('/js/book_selector.js', __FILE__), array('jquery'));
+}
+
+function helion_scripts() {
+	wp_enqueue_script('widget_kategorie', plugins_url('/js/widget_kategorie.js', __FILE__), array('jquery'));
+}
+
+add_action("admin_init", "helion_admin_scripts");
+add_action("init", "helion_scripts");
+add_action('wp_ajax_helion_book_selector', 'helion_book_picker');
+
+?>
---
+++ lib/shortcodes.php
@@ -0,0 +1,214 @@
+<?php
+
+/**
+ * Tu są shortcody:
+ * [helion_ksiazka]
+ * [helion_link]
+ * [helion_wyniki_wyszukiwania]
+ */
+
+add_shortcode( 'helion_ksiazka', 'helion_ksiazka' );
+add_shortcode( 'helion_link', 'helion_link' );
+add_shortcode( 'helion_wyniki_wyszukiwania', 'helion_wyniki_wyszukiwania' );
+
+add_action('wp_print_styles', 'helion_shortcode_styles');
+
+function helion_shortcode_styles() {
+	wp_register_style('helion-shortcodes', plugins_url("css/shortcodes.css", dirname(__FILE__)));
+	wp_enqueue_style('helion-shortcodes');
+}
+
+/**
+ * Parametry:
+ * @param ksiegarnia
+ * @param ident
+ * @param okladka rozmiar okładki
+ * @param width szerokość (bez px)
+ * @param float atrybut float (left|right)
+ *
+ */
+function helion_ksiazka($atts) {
+	
+	extract( shortcode_atts( array(
+		'ksiegarnia' => 'helion',
+		'ident' => 'markwy',
+		'okladka' => '181x236',
+		'width' => '',
+		'float' => 'left',
+	), $atts ) );
+	
+	switch($float) {
+		case 'left':
+		case 'lewy':
+		case 'lewo':
+		case 'l':
+			$float = "alignleft";
+			break;
+		case 'right':
+		case 'prawo':
+		case 'prawy':
+		case 'r':
+		case 'p':
+			$float = "alignright";
+			break;
+		default:
+			$float = "";
+			break;
+	}
+	
+	$wymiary = explode("x", $okladka);
+	
+	if(!$width)
+		 // albo rozmiar okładki, albo 200, które większe
+		$width = max($wymiary[0], 200);
+		
+	
+		
+	$book = helion_get_book_info($ksiegarnia, $ident);
+	if($book) {
+		$link = helion_get_link($ksiegarnia, $ident, $cyfra, true);
+		$okladka = helion_get_cover($ksiegarnia, $ident, $okladka);
+		$tytul = $okladka['alt'];
+		$autor = $book['autor'];
+
+		$cont = '<div class="helion-ksiazka ' . "helion-ksiazka-" . $ksiegarnia . "-" . $ident . ' ' . $float . '" style="width: ' . $width . 'px">';
+		$cont .= '<div class="helion_okladka" style="width: ' . $wymiary[0] . 'px;">';
+		$cont .= '<p><a href="' . $link . '" target="_blank" title="' . $tytul . '">';
+		$cont .= '<img src="' . $okladka['src'] . '" alt="' . $okladka['alt'] . '" />';
+		$cont .= '</a></p>';
+		$cont .= '</div>';
+		$cont .= '<div class="helion_meta">';
+		$cont .= '<p class="helion_tytul"><a href="' . $link . '" target="_blank">' . $tytul . '</a></p>';
+		$cont .= '<p class="helion_autor">autor: ' . $autor . '</p>';
+		$cont .= '<p class="helion_isbn">ISBN: ' . $book['isbn'] . '</p>';
+		$cont .= '<p class="helion_format">Format: ' . $book['format'] . ', stron: ' . $book['liczbastron'] . '</p>';
+		$cont .= '<p class="helion_data">Data wydania: ' . $book['datawydania'] . '</p>';
+		$cont .= '<p class="helion_cena">Cena: ' . $book['cenadetaliczna'] . 'zł</p>';
+		$cont .= '<div class="helion-box">';
+		$cont .= '<a href="' . $link . '">kup teraz</a>';
+		$cont .= '</div>';
+		$cont .= '</div>';
+		$cont .= '</div>';
+		
+		return $cont;
+	} else {
+		return "Wystąpił błąd. Nie znaleziono takiej ksiązki.";
+	}
+}
+
+/**
+ * @param ksiegarnia
+ * @param ident
+ * @param cyfra
+ */
+function helion_link($atts, $content = null) {
+	extract( shortcode_atts( array(
+		'ksiegarnia' => 'helion',
+		'ident' => 'markwy',
+		'cyfra' => '',
+	), $atts ) );
+	
+	$book = helion_get_book_info($ksiegarnia, $ident);
+	if($book) {
+		$link = helion_get_link($ksiegarnia, $ident, $cyfra);
+		
+		if($content) {
+			$cont = '<a href="' . $link . '" target="_blank" title="' . $book['tytul'] . '">' . $content . '</a>';
+		} else {
+			$cont = '<a href="' . $link . '" target="_blank" title="' . $book['tytul'] . '">' . $book['tytul'] . '</a>';
+		}
+		
+		return $cont;
+	} else {
+		return "Wystąpił błąd. Nie znaleziono takiej książki.";
+	}
+}
+
+function helion_wyniki_wyszukiwania() {
+
+	$slug = get_option("helion_wyszukiwarka_slug");
+	
+	if($_REQUEST['helion_wyszukiwarka']) {
+	
+		$template = '<div class="helion_wyszukiwarka_wyniki">';
+		
+		$template .= stripslashes(get_option("helion_wyszukiwarka_template"));
+		
+		$wynik = helion_wyszukiwarka($_REQUEST['helion_wyszukiwarka']);
+		
+		if(!$_REQUEST['helion_serp'] && $wynik) {
+
+			$template = helion_parse_template($template, $wynik[0]);
+			
+			$template .= '</div>';
+			
+			return $template;
+			
+		} else if($_REQUEST['helion_serp'] && $wynik) {
+			
+			$dup_idents = array();
+			
+			$lista_wynikow = '<h3>Wyszukano frazę: <em>&ldquo;' . strip_tags($_REQUEST['helion_wyszukiwarka']) . '&rdquo;</em></h3>';
+			
+			$lista_wynikow .= '<ol class="helion_serp">';
+			
+			foreach($wynik as $w) {
+				if(!in_array($w['ident'], $dup_idents)) {
+					$lista_wynikow .= '<li><a href="' . $slug . '/?helion_wyszukiwarka=' . $w['ident'] . '">' . $w['autor'] . " - " . $w['tytul'] . '</a></li>';
+					$dup_idents[] = $w['ident'];
+				}
+			}
+			
+			$lista_wynikow .= '</ol>';
+			
+			return $lista_wynikow;
+			
+		} else {
+		
+			$dup_idents = array();
+			
+			$output = "<p><strong>Nie znaleziono żadnej książki pasującej do zapytania <em>" . strip_tags($_REQUEST['helion_wyszukiwarka']) . "</em>.</strong></p>";
+			$output .= "<p>Być może zainteresuje cię jedna z poniższych pozycji:</p>";
+			
+			$losowe = helion_losowe_ksiazki();
+			
+			$output .= '<ol class="helion_serp">';
+			
+			foreach($losowe as $l) {
+				if(!in_array($l['ident'], $dup_idents)) {
+					$output .= '<li><a href="' . $slug . '/?helion_wyszukiwarka=' . $l['ident'] . '">' . $l['autor'] . " - " . $l['tytul'] . '</a></li>';
+					$dup_idents[] = $l['ident'];
+				}
+			}
+			
+			$output .= '</ol>';
+			$output .= '<p>Możesz także zmienić treść zapytania i poszukać książek w oparciu o inne słowa kluczowe.</p>';
+			
+			return $output;
+		}
+	} else {
+	
+		$dup_idents = array();
+		
+		$output = "<p><strong>Nie znaleziono żadnej książki pasującej do zapytania <em>" . strip_tags($_REQUEST['helion_wyszukiwarka']) . "</em>.</strong></p>";
+		$output .= "<p>Być może zainteresuje cię jedna z poniższych pozycji:</p>";
+		
+		$losowe = helion_losowe_ksiazki();
+		
+		$output .= '<ol class="helion_serp">';
+		
+		foreach($losowe as $l) {
+			if(!in_array($l['ident'], $dup_idents)) {
+				$output .= '<li><a href="' . $slug . '/?helion_wyszukiwarka=' . $l['ident'] . '">' . $l['autor'] . " - " . $l['tytul'] . '</a></li>';
+				$dup_idents[] = $l['ident'];
+			}
+		}
+		
+		$output .= '</ol>';
+		$output .= '<p>Możesz także zmienić treść zapytania i poszukać książek w oparciu o inne słowa kluczowe.</p>';
+		
+		return $output;
+	}
+}
+
+?>
