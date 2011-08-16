3ef9e89052a60f769a12013139a783c64a6119ff
paulpela <paulpela@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-16 10:00:45 +0000

    git-svn-id: https://plugins.svn.wordpress.org/helion-widgets-pro/tags/1.0.0@424130 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/bookstore.php
@@ -0,0 +1,50 @@
+<?php
+
+add_action('wp_print_styles', 'helion_ksiegarnia_styl');
+
+function helion_ksiegarnia_styl() {
+	wp_register_style('helion-ksiegarnia', plugins_url("css/ksiegarnia.css", dirname(__FILE__)));
+	wp_enqueue_style('helion-ksiegarnia');
+}
+
+add_shortcode('helion_ksiegarnia', 'helion_bookstore');
+
+// TODO: jeśli np helion_bookstore_template_category == null, użyj domyślnego
+function helion_bookstore($atts) {
+
+	global $wpdb;
+	
+	if(helion_bookstore_available(get_option("helion_bookstore_ksiegarnia"))) {
+		$template = '<div class="helion_ksiegarnia">';
+
+		switch($_REQUEST['helion_bookstore']) {
+			case 'category':
+				$template .= stripslashes(get_option("helion_bookstore_template_category"));
+				$template .= '</div>';
+				$template = helion_parse_category_template($template, $_REQUEST['id'], $_REQUEST['helion_page']);
+				break;
+			case 'book':
+				$dane = helion_get_book_info(h_validate_bookstore($_REQUEST['ksiegarnia']), h_validate_ident($_REQUEST['ident']));
+				if(empty($dane)) {
+					$template .= "<p>Ta książka nie jest w tej chwili w sprzedaży.</p>";
+				} else {
+					$template .= stripslashes(get_option("helion_bookstore_template_book"));
+				}
+				$template .= '</div>';
+				$template = helion_parse_template($template, $dane);
+				break;
+			default:
+				$template .= stripslashes(get_option("helion_bookstore_template_main"));
+				$template .= '</div>';
+				$template = helion_parse_bookstore_template($template);
+				break;
+		}
+		
+		return $template;
+		
+	} else {
+		return "<p>Wystąpił błąd. Moduł księgarni nie może odnaleźć danych o książkach z tej księgarni. Sprawdź, czy wybrana przez ciebie księgarnia nie została całkowicie wyłączona w menu Helion->Helion.</p>";
+	}
+}
+
+?>
