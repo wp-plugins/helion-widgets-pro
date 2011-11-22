1394b96bbb9cfeca731bd92acf98f8e6a465f7b4
paulpela <paulpela@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-22 16:09:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/helion-widgets-pro/tags/1.0.1@466076 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/cache.php
@@ -0,0 +1,284 @@
+<?php
+
+function helion_get_cover($bookstore, $ident, $size) {
+	
+	$bookstore = h_validate_bookstore($bookstore);
+	$ident = h_validate_ident($ident);
+	$size = h_validate_size($size);
+	
+	if(!$ident || !$bookstore || !$size) return false;
+	
+	global $wpdb;
+	
+	if(file_exists(ABSPATH . "wp-content/helion-cache/$bookstore/$size/$ident.jpg")) {
+		// todo: update usage
+		$src = home_url("/wp-content/helion-cache/$bookstore/$size/$ident.jpg");
+	} else {
+		$src = "http://$bookstore.pl/okladki/$size/$ident.jpg";
+		if(get_option("helion_current_cache_size") <= get_option("helion_cache_user")) {
+			switch(helion_detect_connection_method()) {
+				case 'fopen':
+					if(copy($src, 
+						ABSPATH . "wp-content/helion-cache/$bookstore/$size/$ident.jpg")) {
+						$src = home_url("/wp-content/helion-cache/$bookstore/$size/$ident.jpg");
+					} else {
+						$src = home_url("/wp-content/helion-cache/$bookstore/$size/$ident.jpg");
+					}
+					break;
+				case 'curl':
+					$out = fopen(ABSPATH . 
+						"wp-content/helion-cache/$bookstore/$size/$ident.jpg", 'wb');
+
+					if($out) {
+						$ch = curl_init();
+						curl_setopt($ch, CURLOPT_FILE, $out);
+						curl_setopt($ch, CURLOPT_HEADER, 0);
+						curl_setopt($ch, CURLOPT_URL, $src);
+						curl_exec($ch);
+						curl_close($ch); 
+						fclose($out);
+						$src = home_url("/wp-content/helion-cache/$bookstore/$size/$ident.jpg");
+					} else {
+						$src = "http://$bookstore.pl/okladki/$size/$ident.jpg";
+					}
+					break;
+				default:
+					$src = "http://$bookstore.pl/okladki/$size/$ident.jpg";
+					break;
+			}
+		} else {
+			$src = "http://$bookstore.pl/okladki/$size/$ident.jpg";
+		}
+	}
+	
+	$dimensions = explode("x", $size);
+
+	return array(
+		"src" => $src,
+		"bookstore" => $bookstore,
+		"ident" => $ident,
+		"width" => $dimensions[0],
+		"height" => $dimensions[1],
+		"alt" => helion_get_book_title($bookstore, $ident),
+		);
+}
+
+function helion_is_cover_cached($bookstore, $ident, $size) {
+
+	$bookstore = h_validate_bookstore($bookstore);
+	$ident = h_validate_ident($ident);
+	$size = h_validate_size($size);
+	
+	if(!$ident || !$bookstore || !$size) return false;
+	
+	if(file_exists(ABSPATH . "wp-content/helion-cache/$bookstore/$size/$ident.jpg")) {
+		// update usage counter
+		return true;
+	} else {
+		// cache it
+		return false;
+	}
+}
+
+/**
+ * Returns current cache size
+ * @return int cache size in kiB
+ */
+function helion_get_current_cache_size() {
+	$total = helion_dirsize(ABSPATH . "wp-content/helion-cache");
+	$current = $total['size'] / 1024;
+	
+	return $current;
+}
+
+function h_disabled_shell_exec() {
+	$disabled_functions = @ini_get('disable_functions');
+	
+	if(!empty($disabled_functions)) {
+		$arr = explode(',', $disabled_functions);
+		if(in_array("shell_exec", $arr)) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
+
+function helion_dirsize($path) {
+	$totalsize = 0;
+	$totalcount = 0;
+	$dircount = 0;
+	if($handle = opendir($path)) {
+		while(false !== ($file = readdir($handle))) {
+		  $nextpath = $path . '/' . $file;
+		  if ($file != '.' && $file != '..' && !is_link ($nextpath)) {
+			if (is_dir ($nextpath)) {
+				  $dircount++;
+				  $result = helion_dirsize($nextpath);
+				  $totalsize += $result['size'];
+				  $totalcount += $result['count'];
+				  $dircount += $result['dircount'];
+			}
+			elseif (is_file ($nextpath)) {
+			  $totalsize += filesize ($nextpath);
+			  $totalcount++;
+			}
+		  }
+		}
+	  }
+	  closedir ($handle);
+	  $total['size'] = $totalsize;
+	  $total['count'] = $totalcount;
+	  $total['dircount'] = $dircount;
+	  return $total;
+} 
+
+/**
+ * Suggests a cache size based on chosen bookstores and cover sizes.
+ * Configured to return an approx. amount needed for storing 1000 covers
+ * of largest size for every bookstore selected.
+ *
+ * @return int cache size in MiB
+ */
+function helion_suggested_cache_size() {
+
+	// in kiB
+	$cover_weight = array(
+		"326x466" => "30",
+		"181x236" => "22",
+		"125x163" => "11",
+		"120x156" => "10",
+		"90x119" => "7",
+		"88x115" => "6",
+		"72x95" => "5",
+		"65x85" => "4",
+		);
+		
+	// TODO: check different places to find used cover sizes
+	// default: 181x236
+	$largest_cover = "181x236";
+	
+	// TODO: check how many different cover sizes have been selected
+	// default: 3
+	$cover_sizes = 3;
+	
+	// check how many bookstores the user selected
+	// default: 2
+	$bookstores = array_count_values(get_option("helion_bookstores"));
+	
+	if($bookstores["1"] != 0) {
+		$bookstores_selected = $bookstores["1"];
+	} else {
+		$bookstores_selected = 2;
+	}
+		
+	// default: 128 MiB
+	return (int) $cover_weight[$largest_cover] * $bookstores_selected * $cover_sizes * 1000 / 1024;
+}
+
+function rrmdir($dir) {
+	if(is_dir($dir)) {
+		$objects = scandir($dir);
+		foreach ($objects as $object) {
+			if($object != "." && $object != "..") {
+				if(filetype($dir . "/" . $object) == "dir") {
+					rrmdir($dir . "/" . $object); 
+				} else {
+					unlink($dir . "/" . $object);
+				}
+			}
+		}
+		reset($objects);
+		rmdir($dir);
+	}
+}
+
+function helion_clear_cache() {
+	rrmdir(ABSPATH . "wp-content/helion-cache");
+}
+
+function helion_setup_cache() {
+	$bookstores = array("helion", "sensus", "onepress", "septem", "ebookpoint");
+	$covers = array("326x466", "181x236", "125x163", "120x156", "90x119", "88x115", "72x95", "65x85");
+	
+	@mkdir(ABSPATH . "wp-content/helion-cache", 0775);
+	foreach($bookstores as $bookstore) {
+		@mkdir(ABSPATH . "wp-content/helion-cache/" . $bookstore, 0775);
+		foreach($covers as $cover) {
+			@mkdir(ABSPATH . "wp-content/helion-cache/" . $bookstore . "/" . $cover, 0775);
+		}
+	}
+	
+	@mkdir(ABSPATH . "wp-content/helion-cache/xml");
+}
+
+/**
+ * Retrieve info about cached cover from the database
+ *
+ * TODO: new field in array: is cover phisically in the cache?
+ *
+ * @param bookstore helion|sensus|onepress etc.
+ * @param ident ident parameter from Helion
+ * @param size ex. 65x123
+ * @return array or false if no info in db
+ */
+function helion_cache_cover_info($bookstore, $ident, $size) {
+
+	$bookstore = h_validate_bookstore($bookstore);
+	$ident = h_validate_ident($ident);
+	$size = h_validate_size($size);
+	
+	if(!$ident || !$bookstore || !$size) return false;
+	
+	global $wpdb;
+	
+	$result = $wpdb->get_row($wpdb->prepare("SELECT * FROM " . $wpdb->prefix . 
+		"helion_cover_cache WHERE bookstore = $bookstore AND ident = $ident " . 
+		"AND size = $size"), ARRAY_A);
+
+	if($result) {
+		return array(
+			"bookstore" => $bookstore,
+			"ident" => $ident,
+			"size" => $size,
+			"usage" => $result['usage'],
+			"to_be_cached" => $result['to_be_cached'],
+			"cached_date" => $result['cached_date'],
+			);
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Cronjobs
+ */
+
+function helion_cron_cache_covers() {
+
+}
+
+function helion_cron_remove_covers() {
+
+}
+
+function helion_reset_cache() {
+	$current = (int) helion_get_current_cache_size();
+	$user = (int) get_option("helion_cache_user");
+	if($current > $user) {
+		helion_clear_cache();
+		helion_setup_cache();
+		update_option("helion_current_cache_size", 0);
+	}
+	
+	// sprawdź ponownie za miesiąc
+	wp_schedule_single_event(time() + 30 * 24 * 60 * 60, 'helion_reset_cache');
+}
+
+function helion_cron_cache_size() {
+	update_option("helion_current_cache_size", (int) helion_get_current_cache_size());
+}
+
+?>
---
+++ widgets/single-book.php
@@ -0,0 +1,188 @@
+<?php
+
+add_action( 'widgets_init', 'helion_load_widget_single_book' );
+
+function helion_load_widget_single_book() {
+	register_widget( 'Helion_Widget_Single_Book' );
+}
+
+class Helion_Widget_Single_Book extends WP_Widget {
+	
+	function Helion_Widget_Single_Book() {
+		$widget_ops = array( 'classname' => 'helion_widget_single_book', 'description' => 'Widget wyświetlający jedną, wybraną przez użytkownika książkę', 'okladka' => '120x156' );
+
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'helion-widget-single-book' );
+
+		$this->WP_Widget( 'helion-widget-single-book', 'Helion - Wybrana Książka', $widget_ops, $control_ops );
+	}
+	
+	function widget($args, $instance) {
+		extract( $args );
+		
+		if(!$instance['bookstore'] || !$instance['ident']) {
+			echo $before_widget;
+			echo $before_title . $instance['title'] . $after_title;
+		?>
+			<p><strong>Wystąpił błąd.</strong> Nie wybrano żadnej książki w konfiguracji widgetu.</p>
+		<?php
+			echo $after_widget;
+		} else {
+			$book = helion_get_book_info($instance['bookstore'], $instance['ident']);
+			
+			if(!$book) {
+				echo $before_widget;
+				echo $before_title . $instance['title'] . $after_title;
+				?>
+				<p>Nie było możliwe pobranie danych na temat książki.</p>
+				<?php
+				echo $after_widget;
+			} else {
+				$dokoszyka = helion_get_link($instance['bookstore'], $book['ident'], $instance['cyfra'], true);
+					
+				if($instance['koszyk']) {
+					$link = $dokoszyka;
+				} else {
+					$link = helion_get_link($instance['bookstore'], $book['ident'], $instance['cyfra']);
+				}
+				
+				$okladka = helion_get_cover($instance['bookstore'], $instance['ident'], $instance['cover']);
+				$tytul = $book['tytul'];
+				$autor = $book['autor'];
+			
+				echo $before_widget;
+				echo $before_title . $instance['title'] . $after_title;
+				?>
+				<div class="helion_okladka" style="width: <?php echo $okladka['width']; ?>px;">
+					<a href="<?php echo $link; ?>" target="_blank" title="<?php echo $tytul; ?>">
+						<img src="<?php echo $okladka['src']; ?>" alt="<?php echo $okladka['alt']; ?>" />
+					</a>
+				</div>
+				<div class="helion_meta" style="width: <?php echo $okladka['width'] + 70; ?>px;">
+					<?php if($instance['tytul']) { ?>
+					<p class="helion_tytul"><a href="<?php echo $link; ?>" target="_blank"><?php echo $tytul; ?></a></p>
+					<?php } ?>
+					<?php if($instance['autor']) { ?>
+						<p class="helion_autor">autor: <?php echo $autor; ?></p>
+					<?php } ?>
+					<?php if($instance['cena']) { ?>
+					<p class="helion_cena">Cena: <?php echo $book['cena']; ?> zł</p>
+					<?php } ?>
+					<?php if($instance['dodatkowe']) { ?>
+						<?php 
+							if($book['nowosc']) {
+								$dod[] = '<img src="http://helion.pl/img/nowosc.gif" alt="nowość" />';
+							}
+							if($book['bestseller']) {
+								$dod[] = '<img src="http://helion.pl/img/bestseller.gif" alt="bestseller" />';
+							}
+							
+							if(!empty($dod)) { ?>
+								<p class="helion_dodatkowe">
+							<?php
+								echo join(" ", $dod);
+							?>
+								</p>
+							<?php
+							}
+						?>
+					<?php } ?>
+					<?php if($instance['przycisk']) { ?>
+						<div class="helion-box"><a href="<?php echo $dokoszyka; ?>">kup teraz</a></div>
+					<?php } ?>
+				</div>
+				<?php
+				echo $after_widget;
+			}
+		}
+	}
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['cover'] = strip_tags( $new_instance['cover'] );
+		$instance['cyfra'] = strip_tags( $new_instance['cyfra'] );
+		$instance['tytul'] = strip_tags( $new_instance['tytul'] );
+		$instance['autor'] = strip_tags( $new_instance['autor'] );
+		$instance['cena'] = strip_tags( $new_instance['cena'] );
+		$instance['dodatkowe'] = strip_tags( $new_instance['dodatkowe'] );
+		$instance['bookstore'] = strip_tags( $new_instance['bookstore'] );
+		$instance['ident'] = strip_tags( $new_instance['ident'] );
+		$instance['przycisk'] = strip_tags( $new_instance['przycisk'] );
+		$instance['koszyk'] = strip_tags( $new_instance['koszyk'] );
+
+		return $instance;
+	}
+	
+	function form( $instance ) {
+		$defaults = array( 'title' => 'Polecana książka', 'cover' => '125x163' );
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$selected = ' selected="selected" ';
+		$checked = ' checked="checked" ';
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Tytuł:</label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" class="widefat" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'bookstore' ); ?>">Księgarnia:</label>
+			<select id="<?php echo $this->get_field_id( 'bookstore' ); ?>" name="<?php echo $this->get_field_name( 'bookstore' ); ?>" class="widefat">
+			<?php
+				foreach(get_option("helion_bookstores") as $bs => $sel) {
+				?>
+					<option <?php if($instance['bookstore'] == $bs) echo $selected; ?> value="<?php echo $bs; ?>"><?php echo ucfirst($bs); ?></option>
+				<?php
+				}
+			?>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'ident' ); ?>">Ident:</label>
+			<input id="<?php echo $this->get_field_id( 'ident' ); ?>" name="<?php echo $this->get_field_name( 'ident' ); ?>" value="<?php echo $instance['ident']; ?>" class="widefat" />
+		</p>
+		<p><small><strong>Ident:</strong> Jest to indywidualny kod identyfikujący każdą książkę w księgarniach Grupy Wydawniczej Helion. Kod ten możesz poznać, sprawdzając adres strony internetowej, na której znajdują się informacje o książce - znajduje się on pod koniec adresu URL. Możesz także skorzystać z wyszukiwarki przeznaczonej dla widgetu Losowa książka - możesz wyszukiwać według autora lub tytułu.</small></p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'cyfra' ); ?>">Cyfra:</label>
+			<input id="<?php echo $this->get_field_id( 'cyfra' ); ?>" name="<?php echo $this->get_field_name( 'cyfra' ); ?>" value="<?php echo $instance['cyfra']; ?>" class="widefat" />
+		</p>
+		<p><small><strong>Cyfra:</strong> Jest to dodatkowy (nieobowiązkowy) parametr, dzięki któremu możesz zbadać skuteczność kampanii. Jeśli prowadzisz dwie strony, na jednej możesz ustawić cyfrę 1 a na drugiej cyfrę 2 - gdy ktoś dokona zakupu, wówczas otrzymasz maila w którym oprócz informacji o zakupach zostanie przekazana ta cyfra i będziesz wiedział, z jakiej strony został dokonany zakup. </small></p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'cover' ); ?>">Rozmiar okładki:</label>
+			<select id="<?php echo $this->get_field_id( 'cover' ); ?>" name="<?php echo $this->get_field_name( 'cover' ); ?>" class="widefat" style="width:100%;">
+				<option <?php if($instance['cover'] == "65x85") echo $selected; ?>>65x85</option>
+				<option <?php if($instance['cover'] == "72x95") echo $selected; ?>>72x95</option>
+				<option <?php if($instance['cover'] == "72x95") echo $selected; ?>>72x95</option>
+				<option <?php if($instance['cover'] == "90x119") echo $selected; ?>>90x119</option>
+				<option <?php if($instance['cover'] == "120x156") echo $selected; ?>>120x156</option>
+				<option <?php if($instance['cover'] == "125x163") echo $selected; ?>>125x163</option>
+				<option <?php if($instance['cover'] == "181x236") echo $selected; ?>>181x236</option>
+				<option <?php if($instance['cover'] == "326x466") echo $selected; ?>>326x466</option>
+			</select>
+		</p>
+		<p>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'tytul' ); ?>" name="<?php echo $this->get_field_name( 'tytul' ); ?>" <?php if($instance['tytul']) echo $checked; ?> />
+			<label for="<?php echo $this->get_field_id( 'tytul' ); ?>">Wyświetlać tytuł książki?</label>
+			<br/>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'autor' ); ?>" name="<?php echo $this->get_field_name( 'autor' ); ?>" <?php if($instance['autor']) echo $checked; ?> />
+			<label for="<?php echo $this->get_field_id( 'autor' ); ?>">Wyświetlać autora książki?</label>
+			<br/>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'cena' ); ?>" name="<?php echo $this->get_field_name( 'cena' ); ?>" <?php if($instance['cena']) echo $checked; ?> />
+			<label for="<?php echo $this->get_field_id( 'cena' ); ?>">Wyświetlać cenę książki?</label>
+			<br/>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'dodatkowe' ); ?>" name="<?php echo $this->get_field_name( 'dodatkowe' ); ?>" <?php if($instance['dodatkowe']) echo $checked; ?> />
+			<label for="<?php echo $this->get_field_id( 'dodatkowe' ); ?>">Wyświetlać dodatkowe informacje o książce (nowość, bestseller itp.)?</label>
+			<br/>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'przycisk' ); ?>" name="<?php echo $this->get_field_name( 'przycisk' ); ?>" <?php if($instance['przycisk']) echo $checked; ?> />
+			<label for="<?php echo $this->get_field_id( 'przycisk' ); ?>">Wyświetlać przycisk "Kup Teraz"?</label>
+			<br/>
+		</p>
+		<p>
+			<input type="checkbox" id="<?php echo $this->get_field_id( 'koszyk' ); ?>" name="<?php echo $this->get_field_name( 'koszyk' ); ?>" <?php if($instance['koszyk']) echo $checked; ?> />
+			<label for="<?php echo $this->get_field_id( 'koszyk' ); ?>">Zaznacz, aby link w okładce i tytule dodawał książkę do koszyka, a nie prowadził do strony z opisem książki</label>
+			<br/>
+		</p>
+
+		<?php
+	}
+}
+?>
